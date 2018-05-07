add02039361d728f2a85aa8d89fac3878f63e195
Dara Javaherian <darajavaherian@gmail.com>
2018-05-08 01:08:13 +0200

    get it actually working

--- src/App.js
+++ src/App.js
@@ -8,21 +8,87 @@ import Cesium from "cesium/Cesium";
 Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0MGU4YTVkYy0zMGRlLTRmNDEtYjkxNy05YjExZjIxOThkMTYiLCJpZCI6NzM5LCJpYXQiOjE1MjU3MzEzODZ9.WWg4TUtX6CUiapgW4DyO6uRoSE1APtGJkUOK6Fvzcmo';
 
 class App extends Component {
-  removeElementByClass(className) {
-    // Get the element by their class name
-    var elements = document.getElementsByClassName(className);
-    
-    // Now remove them
 
-    for (var i = 0; i < elements.length; i++) {
+  constructor() {
+    super();
+
+    this.createWhiteGlobe = this.createWhiteGlobe.bind(this);
+    
+    this.createWhiteGlobe();
+  }
+
+  createWhiteGlobe() {
+        let instances = [];
+
+    // for (let lon = -180.0; lon < 180.0; lon += 5.0) {
+    //   for (let lat = -89.9; lat < 89.0; lat += 5.0) {
+        instances.push(new Cesium.GeometryInstance({
+          geometry : new Cesium.RectangleGeometry({
+            rectangle : Cesium.Rectangle.fromDegrees(-180, -90, 180, 90),
+            vertexFormat: Cesium.PerInstanceColorAppearance.VERTEX_FORMAT
+          }),
+          attributes : {
+            color : Cesium.ColorGeometryInstanceAttribute.fromColor(new Cesium.Color(1, 1, 1, 1))
+          }
+        }));
+
+    this.whiteGlobe = new Cesium.Primitive({
+      geometryInstances : instances,
+      appearance : new Cesium.PerInstanceColorAppearance()
+    });
+  }
+
+  addWhiteSphere() {
+    let scene = this.viewer.scene;
+
+
+    //   }
+    // }
+
+    scene.primitives.add(this.whiteGlobe);
+  }
+
+  removeElementByClass(className) {
+    let elements = document.getElementsByClassName(className);
+    
+    for (let i = 0; i < elements.length; i++) {
       elements[i].parentNode.removeChild(elements[i]);
     }
   }
 
+  addZoomListener() {
+    let scene = this.viewer.scene;
+
+    let lastHeight;
+    scene.preRender.addEventListener(() => {
+        let camera = scene.camera;
+        let height = camera.positionCartographic.height;
+        if (lastHeight !== parseInt(height)) {
+            //Camera height has changed
+            console.log(height);
+            console.log(lastHeight);
+            lastHeight = parseInt(height);
+            console.log(lastHeight);
+            if (lastHeight < 600000) {
+              scene.primitives.remove(this.whiteGlobe);
+              this.createWhiteGlobe();
+            } else {
+              if (!scene.primitives.contains(this.whiteGlobe)) {
+                scene.primitives.add(this.whiteGlobe);
+              }
+            }
+        }
+    });
+  }
+
   componentDidMount() {
-    let viewer = new Cesium.Viewer('cesiumContainer', {
+    this.viewer = new Cesium.Viewer('cesiumContainer', {
         // Show Columbus View map with Web Mercator projection
     });
+
+    this.addWhiteSphere();
+
+    this.addZoomListener();
 
     let classNames = [
       'cesium-viewer-timelineContainer',
@@ -42,7 +108,7 @@ class App extends Component {
 
   render() {
     return (
-      <div id='cesiumContainer'>
+      <div id='cesiumContainer' >
       </div>
     );
   }
